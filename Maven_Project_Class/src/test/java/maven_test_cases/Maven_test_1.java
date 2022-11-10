9f1eaf51f4b3acce65927ab1c20d9ee08a1f3b89
osurugopi <117894957+osurugopi@users.noreply.github.com>
2022-11-10 19:14:47 +0530

    selenium

---
+++ Maven_Project_Class/src/test/java/maven_test_cases/Maven_test_1.java
@@ -0,0 +1,37 @@
+package maven_test_cases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+
+public class Maven_test_1 {
+	
+	WebDriver driver;
+	
+  @Test
+  public void Google() {
+	  driver.get("https://www.google.com/");
+	  driver.findElement(By.name("q")).sendKeys("hello");
+  }
+  
+  @Test
+  public void Facebook() {
+	  driver.get("https://www.facebook.com/");
+  }
+  
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.chrome.driver", "F:\\Lib\\64 Bit\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
